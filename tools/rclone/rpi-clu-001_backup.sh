a6edbd372b0cc13713f390612ec166a7244ce9a7
annorberg98 <antonnorberg98@gmail.com>
2023-11-10 17:03:21 +0100

    Added backup script

---
+++ tools/rclone/rpi-clu-001_backup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+rclone sync -v --create-empty-src-dirs /portainer/Files/AppData/Config/ r2:selfhosted-backup --config /home/pi/.config/rclone/rclone.conf 
+--exclude grafana/** --exclude prometheus/** --exclude freshrss/log/** 
+--exclude freshrss/www/freshrss/data/** --exclude trilium/sessions/** --exclude trilium/log/**
+--exclude PiHole/*.db --exclude *.log
+
+webhook_url="https://discord.com/api/webhooks/1166707372350251049/0V-vunc1hnwgRSu-NP3crshdxYNh0UuyC4gY5CzcZ0OR1PBEdPzbNdphv6Y7GlikT7Qc"
+
+# Check if the RClone command was successful (exit code 0)
+if [ $? -eq 0 ]; then
+    # If the RClone command succeeded, ping the webhook with a success message
+    webhook_url="https://discord.com/api/webhooks/1166707372350251049/0V-vunc1hnwgRSu-NP3crshdxYNh0UuyC4gY5CzcZ0OR1PBEdPzbNdphv6Y7GlikT7Qc"
+    message="RClone Backup of rpi-clu-001 to CloudFlare R2 finished succesfully."  # Set your desired message
+else
+    # If the RClone command failed, ping the webhook with an error message
+    webhook_url="https://discord.com/api/webhooks/1166707372350251049/0V-vunc1hnwgRSu-NP3crshdxYNh0UuyC4gY5CzcZ0OR1PBEdPzbNdphv6Y7GlikT7Qc"
+    message="RClone Backup of rpi-clu-001 to CloudFlare R2 Failed."  # Set your desired message
+fi
+
+# Send the message to the webhook
+if [ -n "$message" ]; then
+    curl -X POST -H "Content-Type: application/json" -d "{\"content\": \"$message\"}" "$webhook_url"
+else
+    echo "Message is empty."
+fi
